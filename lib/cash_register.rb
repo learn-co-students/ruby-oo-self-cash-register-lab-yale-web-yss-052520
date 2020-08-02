1d5c278794104aaddc9a223c8a49dbf67f5472e2
larissanguyen <larissa.h.nguyen@gmail.com>
2020-08-01 23:42:42 -0400

    keeps saying uninitialized constant CashRegister

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,7 @@
+def CashRegister
+	attr_accessor :total
 
+	def initialize(total = 0)
+		@total = total
+	end
+end
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -1,3 +1,6 @@
+require_relative "spec_helper"
+require_relative '../lib/cash_register'
+
 describe 'CashRegister' do
   let(:cash_register) { CashRegister.new }
   let(:cash_register_with_discount) { CashRegister.new(20) }
